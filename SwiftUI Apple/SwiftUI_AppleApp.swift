8436e2e61f67eae7eb7abbcaf9a7677c0ef6c8d4
Dmitrii Lobanov <oftkor@gmail.com>
2022-07-02 00:31:10 +0300

    Initial Commit

---
+++ SwiftUI Apple/SwiftUI_AppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AppleApp.swift
+//  SwiftUI Apple
+//
+//  Created by Dmitrii Lobanov on 02.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
